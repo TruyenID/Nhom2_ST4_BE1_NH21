5e633c1f0bfe4392898271a4c8c0daf9eb074d8d
TranDangKhoa38 <thaytrokami@gmail.com>
2021-12-08 09:59:29 +0700

    login admin

--- admin/header.php
+++ admin/header.php
@@ -47,7 +47,7 @@ $manu = new Manufacture;
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="index.php" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
@@ -171,6 +171,11 @@ $manu = new Manufacture;
       <li class="nav-item">
         <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
           <i class="fas fa-th-large"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"  href="logout.php" role="button">
+          Logout
         </a>
       </li>
     </ul>
